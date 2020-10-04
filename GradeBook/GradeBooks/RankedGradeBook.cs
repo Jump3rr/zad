8941e2dfbac73517821a0c36b7a98eb831eab815
Jump3rr <theneonxd@gmail.com>
2020-10-04 16:00:19 +0200

    11accept

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var progProcentowy = (int)Math.Ceiling(Students.Count * 0.2);
+            var stopnie = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (stopnie[progProcentowy - 1] <= averageGrade)
+                return 'A';
+            else if (stopnie[(progProcentowy * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (stopnie[(progProcentowy * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (stopnie[(progProcentowy * 4) - 1] <= averageGrade)
+                return 'D';
+
+            return 'F';
+
         }
     }
 }
