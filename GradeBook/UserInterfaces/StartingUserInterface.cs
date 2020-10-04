20fba1e92958701a7c84c23594a7a0e13dfd3c50
Jump3rr <theneonxd@gmail.com>
2020-10-04 19:19:49 +0200

    23 accept

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,25 +43,30 @@ namespace GradeBook.UserInterfaces
             {
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
-            }
+            }   
             var name = parts[1];
-            BaseGradeBook gradeBook;
             var type = parts[2].ToLower();
-            var isWeighted = bool.Parse(parts[3]);
-            if (type == "standard")
+            var weighted = bool.Parse(parts[3]);
+            BaseGradeBook gradeBook;
+            /*if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name, isWeighted);
+                gradeBook = new StandardGradeBook(name, weighted);
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name, isWeighted);
-            }
+                gradeBook = new RankedGradeBook(name, weighted);
+            }*/
+            if (type == "standard")
+                gradeBook = new StandardGradeBook(name, weighted);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name, weighted);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
             }
-
+            Console.WriteLine("Created gradebook {0}.", name);
+            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
